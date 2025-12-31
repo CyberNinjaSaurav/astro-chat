323ab5cf25d5336245b7e6c9184b4f713c385fe5
Saurav Sapkota <saurav2021ptu@gmail.com>
2025-12-31 21:08:24 +0530

    commit

--- server/routes/chat_routes.py
+++ server/routes/chat_routes.py
@@ -1,12 +1,21 @@
 from flask import Blueprint, request, jsonify
-
+from services.chat_service import chat_with_memory
+from utils.jwt_utils import get_current_user
 
 chat_bp = Blueprint("chat", __name__)
 
 @chat_bp.route("/", methods=["POST"])
 def chat():
+    user = get_current_user(request)
     data = request.json
-    return jsonify({
-        "reply": "AI response will come here",
-        "input": data
-    })
+
+    if not data or "message" not in data:
+        return jsonify({"error": "Message required"}), 400
+    
+    reply = chat_with_memory(
+        user_email=user["email"],
+        user_message=data["message"]
+    )
+
+    return jsonify({"reply": reply})
+
